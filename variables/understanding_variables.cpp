55d62d0b475960b5139390b001824db331558414
iamAayushMishra <fly2Aayush@gmail.com>
2023-06-01 16:35:15 +0530

    understanding variables

---
+++ variables/understanding_variables.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+// Variables are containers used to store values 
+
+int main() {
+
+    //int type variable
+    int number;
+    number = 15; //asigned value
+
+    //float
+    float varFloat = 15.5; //value can be assigned while defining
+
+    //charcter
+    char myCharVar = 'D';
+
+    //str
+    string myStr = "Cpp";
+
+    //const = value cannot be changed
+    const float _pi = 3.14;
+
+    // printing these variables
+    cout<<number<<endl;
+    cout<<varFloat<<endl;
+    cout<<myCharVar<<endl;
+    cout<<myStr<<endl;
+    cout<<_pi<<endl;
+
+}
+
+
+
+
+
+    
+    
