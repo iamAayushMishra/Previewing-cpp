86190a261c5e28250f1ee6dfbd52745da6448f74
iamAayushMishra <fly2Aayush@gmail.com>
2023-06-02 15:55:07 +0530

    operators + maths

---
+++ operators/takingUserInput.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num1,num2;
+    cout<<"Enter two numbers separated by space: ";
+    cin>>num1>>num2;
+    cout<<"num1 + num2 = "<<(num1+num2)<<endl;
+}
