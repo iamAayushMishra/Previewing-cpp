02f2e752718b5113656653124c5b0a270dab278c
iamAayushMishra <fly2Aayush@gmail.com>
2023-06-07 18:58:17 +0530

    creating first array

--- arrays/01_arrays.cpp
+++ arrays/01_arrays.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
 
+    //specified type/no of items in array
     string companies[4] ={"Audi", "BMW", "Apple", "Microsoft"};
 
     cout<<companies[1]; //prints BMW
