fc2a91058477fad0d4efe1977c998200ee20a625
iamAayushMishra <fly2Aayush@gmail.com>
2023-06-15 14:20:39 +0530

    hollow pattern

--- patterns/02_pattern.cpp
+++ patterns/02_pattern.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 /*
-task : print a completely hollow star pattern 
+task : print a completely filled star pattern 
 
 eg - 
     Enter no of rows and column: 5 3
@@ -16,14 +16,20 @@ int main() {
     cout << "Enter no of rows and column: ";
     cin >> row >> column;
 
+    // Loop over each column
     for (int i = 0; i < column; i++) {
+        // Loop over each row
         for (int j = 0; j < row; j++) {
+            // Check if current position is on the edges of the pattern
             if (i == 0 || i == column - 1 || j == 0 || j == row - 1) {
+                // Print a "*" character if on the edges
                 cout << "*";
             } else {
+                // Print a space character for the hollow center
                 cout << " ";
             }
         }
+        // Move to the next line after each row
         cout << endl;
     }
 
